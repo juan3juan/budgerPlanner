46d607dfcb16d4605846d85aaeae2e940269cf83
jihanzhang <dreck.zhang@gmail.com>
2020-03-14 14:25:10 -0400

    Updates

---
+++ precache-manifest.f22b6df980de8314b3d885bd68c219df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f92e47639f4b1ac2c56ee645faa7682b",
+    "url": "/budgetPlanner/index.html"
+  },
+  {
+    "revision": "282ff2266c46d3f3607d",
+    "url": "/budgetPlanner/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2cadbc717b140424d859",
+    "url": "/budgetPlanner/static/css/main.97e31b9b.chunk.css"
+  },
+  {
+    "revision": "282ff2266c46d3f3607d",
+    "url": "/budgetPlanner/static/js/2.c4949ae9.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/budgetPlanner/static/js/2.c4949ae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cadbc717b140424d859",
+    "url": "/budgetPlanner/static/js/main.4c3e4800.chunk.js"
+  },
+  {
+    "revision": "cbcb839c06f49b587560",
+    "url": "/budgetPlanner/static/js/runtime-main.84d2c5dc.js"
+  },
+  {
+    "revision": "fbfa9d7425e7865a700574a4ff5899fa",
+    "url": "/budgetPlanner/static/media/logo.fbfa9d74.png"
+  }
+]);
